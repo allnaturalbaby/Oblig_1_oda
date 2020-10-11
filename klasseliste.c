6e42b4957dfa18911047dc634ba3e0996fcbe515
Viktor Ormestad Larsen <s224285@debbie.usn.no>
2020-10-11 14:34:04 +0200

    syncing finished files

--- klasseliste.c
+++ klasseliste.c
@@ -1,56 +1,89 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "string.h"
 
-
-struct medlemsinfo {
-  char fornavn   [250]; // alle navn bortsett fra etternavn
-  char etternavn [100]; // det siste navnet
-  int  pub_key;         // 0 hvis medlem mangler offentlig noekkel
+struct medlemsinfo { // definerer en type struct medlemsinfo
+	char fornavn   [250]; // alle navn bortsett fra etternavn
+	char etternavn [100]; // det siste navnet
+	int  pub_key;         // 0 hvis medlem mangler offentlig noekkel
 };
 
+int i = 0; // den globale counteren for hvor mange entries i klasselista
+
+// legger til en "type alias" i det globale name spacet
+// kan nå feks si konto_t pub_key = 1, i motsetning til å måtte deklarere
+// struct medlemsinfo pub_key = 1.
 typedef struct medlemsinfo konto_t;
 
-void les_data   (konto_t *klasseliste);
-void skriv_data (konto_t *klasseliste);
-void lese_og_skrive_eksempel();
+void les_data(konto_t *klasseliste);
+void skriv_data(konto_t *klasseliste, char *args);
 
-int main() {
+int main(int argc, char *argv[]) { // evt. int argc, char **argv. holder styr på argumenter fra kommandolinja
+	/*
+	konto_t er et alias for "struct medlemsinfo"
+	klasseliste[200] = [ [person1: {fornavn:, etternavn:, pub_key: }], [person2], osv. ]
+	det blir satt av 200 plasser til personer. personene er av typen medlemsinfo/konto_t
+	*/
+	
+	konto_t klasseliste[200];
+	char *args = NULL;
+	les_data(klasseliste);
 
-  konto_t klasseliste[200];
+	if(argc > 1) {
+		args = argv[1];
+		skriv_data(klasseliste, args);
+	}
+//	skriv_data(klasseliste, args);
 
-  lese_og_skrive_eksempel();
-  les_data  (klasseliste);
-  skriv_data(klasseliste);
-
-  return 0;
+	return 0;
 }
 
 
-void les_data   (konto_t *klasseliste) {
+void les_data(konto_t *klasseliste) {
+	char *txt = NULL; // sørger for at pekeren ikke peker til en ugyldig adresse
+	char *string = NULL; // ^
+	// hvis en peker ikke blir initialisert til NULL kan det være man får en segmenteringsfeil,
+	// da den feks kan peke på en minneadresse man ikke har tilgang til
+	size_t len = 0;
+	
+	while(-1 != (len=getline(&txt, &len, stdin))) {
+		string = strtok(txt, " "); // extracter pub_key statusen
+		klasseliste[i].pub_key = strtol(string, NULL, 10); // konverter til int og setter verdien
 
-  // skriv kode  her
+		string = strtok(NULL, " "); // extracter fornavn
+		strcpy(klasseliste[i].fornavn, string); // kopierer fornavnet til variabelen
+
+		string = strtok(NULL, "\n"); // extracter de siste navna
+		strcpy(klasseliste[i].etternavn, string); // kopierer etternavn(og mellomnavn) til variabelen
+		i++; // inkrementerer den globale counteren slik at skriv_data() vet hvor mange plasser som er i lista
+	}
 }
 
-void skriv_data (konto_t *klasseliste) {
+void skriv_data(konto_t *klasseliste, char *args) {
+//	char *key = "key";
+//	char *fornavn = "fornavn";
+//	char *etternavn = "etternavn";
+	char *m = getenv("KLASSE");
 
-  // skriv kode her
-}
-
-
-void lese_og_skrive_eksempel() {
-
-  /*
-    Leser fra standard inngang, og
-    skriver til standard utgang -- som cat(1)
-    Introduserer:
-    - getline(3)
-  */
-
-  char  *txt = NULL; // peker til lagerplass for innlest linje
-  size_t len = 0;    // lengde på innlest linje
-
-  while ( -1 != ( len=getline( &txt, &len, stdin ) ) )
-    printf("%s", txt);
-
+	if(strcmp("etternavn", args) == 0 || m == "etternavn") {
+		for(int j=0; j<i; j++)
+			printf("%s, %s\t%d\n", klasseliste[j].etternavn, klasseliste[j].fornavn, klasseliste[j].pub_key);
+	}
+	else if(strcmp("fornavn", args) == 0 || m == "fornavn") {
+		for(int j=0; j<i; j++)
+			printf("%s %s\t%d\n", klasseliste[j].fornavn, klasseliste[j].etternavn, klasseliste[j].pub_key);
+	}
+	else if(strcmp("key", args) == 0 || m == "key") {
+		for(int j=0; j<i; j++)
+			printf("%d\t%s %s\n", klasseliste[j].pub_key, klasseliste[j].fornavn, klasseliste[j].etternavn);
+	}
+	else
+		printf("Argumentene er: fornavn, etternavn eller key (eller bruk og endre $KLASSE)\n");
+	
+//	for(int j=0; j<i; j++) { // bruker den globale counteren
+//		printf("%d\t%s %s", klasseliste[j].pub_key, klasseliste[j].fornavn, klasseliste[j].etternavn);
+//		printf("%d\t%s\n", klasseliste[j].pub_key, klasseliste[j].fornavn);
+//		printf("%d\n", klasseliste[j].pub_key);
+//	}
 }
--- lista.sh
+++ lista.sh
@@ -3,6 +3,6 @@
 while read -r userid
 do
 	curl -k -s https://web01.usn.no/~$(printf "$userid" | tr -d s)/pub_key.asc |
-	grep -q "404 Not Found" && printf "0\t" || printf "1\t"
+	grep -q "404 Not Found" && printf "0 " || printf "1 "
 	grep "$userid" /etc/passwd | cut -f5 -d: | tr -d ,
 done < <(members da-oda2001-2020 | tr " " "\n")
